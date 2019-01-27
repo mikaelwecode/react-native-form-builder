7c02c979f9c7b02166b5437ac9e8ddf640da847a
Mikael Carlstein <mikael.carlstein@wecode.se>
2019-01-27 12:00:37 +0100

    Update index.js
    remove placeholder and added title

--- src/fields/textInput/index.js
+++ src/fields/textInput/index.js
@@ -1,8 +1,8 @@
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-import { View, Item, Input, Icon, ListItem, Text } from 'native-base';
-import { Platform } from 'react-native';
-import { getKeyboardType } from '../../utils/methods';
+import PropTypes from 'prop-types'
+import React, {Component} from 'react'
+import {View, Item, Input, Icon, ListItem, Text} from 'native-base'
+import {Platform} from 'react-native'
+import {getKeyboardType} from '../../utils/methods'
 
 export default class TextInputField extends Component {
   static propTypes = {
@@ -10,49 +10,62 @@ export default class TextInputField extends Component {
     theme: PropTypes.object,
     updateValue: PropTypes.func,
     onSummitTextInput: PropTypes.func,
-    ErrorComponent: PropTypes.func,
+    ErrorComponent: PropTypes.func
   }
   handleChange(text) {
-    this.props.updateValue(this.props.attributes.name, text);
+    this.props.updateValue(this.props.attributes.name, text)
   }
   render() {
-    const { theme, attributes, ErrorComponent } = this.props;
-    const inputProps = attributes.props;
-    const keyboardType = getKeyboardType(attributes.type);
+    const {theme, attributes, ErrorComponent} = this.props
+    const inputProps = attributes.props
+    const keyboardType = getKeyboardType(attributes.type)
     return (
-      <ListItem style={{ borderBottomWidth: 0, paddingVertical: 5 }}>
-        <View style={{ flex: 1 }}>
+      <ListItem style={{borderBottomWidth: 0, paddingVertical: 5}}>
+        <View style={{flex: 1}}>
           <View>
-            <Item error={theme.changeTextInputColorOnError ? attributes.error : null}>
-              { attributes.icon &&
-              <Icon color={theme.textInputIconColor} name={attributes.icon} />
-                }
+            <Text>{attributes.label}</Text>
+            <Item
+              error={
+                theme.changeTextInputColorOnError ? attributes.error : null
+              }>
+              {attributes.icon && (
+                <Icon color={theme.textInputIconColor} name={attributes.icon} />
+              )}
               <Input
                 style={{
-                  height: inputProps && inputProps.multiline && (Platform.OS === 'ios' ? undefined : null),
-                  padding: 0,
+                  height:
+                    inputProps &&
+                    inputProps.multiline &&
+                    (Platform.OS === 'ios' ? undefined : null),
+                  padding: 0
                 }}
-                ref={(c) => { this.textInput = c; }}
-                underlineColorAndroid="transparent"
+                ref={(c) => {
+                  this.textInput = c
+                }}
+                underlineColorAndroid='transparent'
                 numberOfLines={3}
-                secureTextEntry={attributes.secureTextEntry || attributes.type === 'password'}
-                placeholder={attributes.label}
+                secureTextEntry={
+                  attributes.secureTextEntry || attributes.type === 'password'
+                }
                 blurOnSubmit={false}
-                onSubmitEditing={() => this.props.onSummitTextInput(this.props.attributes.name)}
+                onSubmitEditing={() =>
+                  this.props.onSummitTextInput(this.props.attributes.name)
+                }
                 placeholderTextColor={theme.inputColorPlaceholder}
                 editable={attributes.editable}
                 value={attributes.value && attributes.value.toString()}
                 keyboardType={keyboardType}
-                onChangeText={text => this.handleChange(text)}
+                onChangeText={(text) => this.handleChange(text)}
                 {...inputProps}
               />
-              { theme.textInputErrorIcon && attributes.error ?
-                <Icon name={theme.textInputErrorIcon} /> : null}
+              {theme.textInputErrorIcon && attributes.error ? (
+                <Icon name={theme.textInputErrorIcon} />
+              ) : null}
             </Item>
           </View>
-          <ErrorComponent {...{ attributes, theme }} />
+          <ErrorComponent {...{attributes, theme}} />
         </View>
       </ListItem>
-    );
+    )
   }
 }
